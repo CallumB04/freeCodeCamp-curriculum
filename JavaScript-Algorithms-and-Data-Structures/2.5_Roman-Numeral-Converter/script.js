d177b2770679d2c5fbbc569eb1f3cee06e04c129
Cal <callumburgoyne04@gmail.com>
2024-09-24 12:33:31 +0100

    Added number conversion - Finished project

--- JavaScript-Algorithms-and-Data-Structures/2.5_Roman-Numeral-Converter/script.js
+++ JavaScript-Algorithms-and-Data-Structures/2.5_Roman-Numeral-Converter/script.js
@@ -3,19 +3,31 @@ const convertBtn = document.getElementById("convert-btn");
 const outputBox = document.getElementById("output-box");
 const output = document.getElementById("output");
 
+// function to change display box for various errors
+function giveError(){
+    outputBox.style.border = "3px solid red";
+    outputBox.style.backgroundColor = "lightpink";
+    outputBox.style.display = "block";
+    output.style.fontSize = "1rem";
+}
+
 function displayOutput() {
     const input = parseInt(numberInput.value);
 
-    if (!numberInput.value || isNaN(input) || input <= 0) {
-        // Giving error if no input is given
-        outputBox.style.border = "3px solid red";
-        outputBox.style.backgroundColor = "lightpink";
-        outputBox.style.display = "block";
-        output.style.fontSize = "1rem";
-        output.style.padding = "15px";
-        output.innerText = "ERROR: Please enter a valid number"
+    // No input / non-numeric value
+    if (!numberInput.value || isNaN(input)) {
+        output.style.padding = "16px";
+        output.innerText = "Please enter a valid number";
+        return giveError();
 
-        return;
+    } else if (input <= 0) {
+        output.style.padding = "8px";
+        output.innerText = "Please enter a number greater than or equal to 1";
+        return giveError();
+    } else if (input >= 4000) {
+        output.style.padding = "8px";
+        output.innerText = "Please enter a number less than or equal to 3999";
+        return giveError();
     }
 
     // resetting output box styling, incase error was given previously
@@ -25,12 +37,20 @@ function displayOutput() {
     output.style.fontSize = "1.4rem";
     output.style.padding = "13px";
     output.innerText = number_to_roman(input);
-
-
 }
 
 function number_to_roman(number) {
+    const numerals = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    let roman = "";
 
+    for (let value of Object.keys(numerals)) {
+        while (number >= numerals[value]) {
+            roman += value;
+            number -= numerals[value];
+        }
+    }
+
+    return roman;
 }
 
 convertBtn.addEventListener("click", displayOutput);
