a1edf7623d0c62e39a5cb1b3b743c8d7d5e5cfa3
Cal <callumburgoyne04@gmail.com>
2024-09-30 17:13:08 +0100

    Added full functionality, ready to implement checking regex

---
+++ JavaScript-Algorithms-and-Data-Structures/3.5_Telephone-Number-Validator/script.js
@@ -0,0 +1,36 @@
+const userInput = document.getElementById("user-input");
+const resultsDiv = document.getElementById("results-div");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+
+function checkNumber(number) {
+    
+}
+
+function displayResult() {
+
+    const isNumber = checkNumber(userInput.value);
+    
+    // Displaying result in results div
+    if (userInput.value.length > 0) {
+        resultsDiv.innerHTML += `
+        <p class="${isNumber ? "valid" : "invalid"}">
+            ${isNumber ? "Valid" : "Invalid"} US number: <span class="checked">${userInput.value}</span>
+        </p>`;
+        resultsDiv.scrollTop = resultsDiv.scrollHeight; // automatically scrolls with inputted numbers
+
+        userInput.value = ""; // clearing user input after check
+    } else {
+        alert("Please provide a phone number"); // alerting if input box is empty
+    }
+}
+
+function clearResults() {
+    resultsDiv.innerHTML = "";
+    userInput.value = "";
+}
+
+
+userInput.addEventListener("keypress", (event) => event.key === "Enter" ? displayResult() : null);
+checkBtn.addEventListener("click", displayResult);
+clearBtn.addEventListener("click", clearResults);
